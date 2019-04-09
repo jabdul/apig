e7994426ad6f31550a1964afaa9a248a5fb65818
jabdul <james.abdul@craftturf.com>
2019-04-09 11:24:33 +0100

    Move questions to endpoint generator to improve code cohesion

--- src/index.js
+++ src/index.js
@@ -1,34 +1,6 @@
-import path from 'path';
 import inquirer from 'inquirer';
-import capitalize from 'capitalize';
 
-import { generator as generateEndpoint } from './endpoint/generator';
-
-const questions = [
-  {
-    type: 'input',
-    name: 'endpoint',
-    message: 'Endpoint name (eg order, account, payment)',
-  },
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Endpoint title (eg Order, Account, Payment)',
-    default: answers => capitalize(answers.endpoint),
-  },
-  {
-    type: 'input',
-    name: 'endpoints',
-    message: () => 'Endpoint pluralisartion (eg orders, accounts, payments)',
-    default: answers => `${answers.endpoint}s`,
-  },
-  {
-    type: 'input',
-    name: 'destination',
-    message: 'Endpopint installation destination',
-    default: () => path.resolve(__dirname, '../', './src')
-  },
-];
+import { generator as generateEndpoint, questions } from './endpoint/generator';
 
 const build = async () => {
   try {
