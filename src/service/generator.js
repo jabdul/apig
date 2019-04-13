c781e3db2e202b698da43d546cbd11c4372deb73
jabdul <james.abdul@craftturf.com>
2019-04-13 17:41:06 +0100

    Add microservice generator

---
+++ src/service/generator.js
@@ -0,0 +1,98 @@
+import Scaffold from 'scaffold-generator';
+import mustache from 'mustache';
+import path from 'path';
+import { pickBy, startsWith } from 'ramda';
+import mkdirp from 'mkdirp';
+
+import config from './config';
+
+export const questions = [
+  {
+    type: 'input',
+    name: 'name',
+    message: 'package.json module name (eg orders, accounts-api, @namespace/payments)',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'package.json description of microservice',
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'package.json author e.g Barney Rubble <b@rubble.com> (http://barnyrubble.tumblr.com/)',
+    default: () => '',
+  },
+  {
+    type: 'input',
+    name: 'giturl',
+    message: 'Git repository url e.g git@github.com:jabdul/crud-api.git#master',
+    default: () => 'git@github.com',
+  },
+  {
+    type: 'input',
+    name: 'directory',
+    message: 'Root directory folder name (eg orders, accounts-api, payments)',
+  },
+  {
+    type: 'input',
+    name: 'mongodb',
+    message: 'Name of MongoDb instance to connect to (eg orders, accounts-api, payments)',
+  },
+  {
+    type: 'input',
+    name: 'port',
+    message: 'Preferred microservice port number',
+    default: () => '4021',
+  },
+  {
+    type: 'input',
+    name: 'destination',
+    message: 'Microservice installation destination path',
+    default: () => path.resolve(__dirname, '../', './src')
+  },
+];
+
+export const generator = function({
+  name,
+  description,
+  author,
+  giturl,
+  directory,
+  mongodb,
+  port,
+  destination
+}) {
+  mkdirp(destination, (err) => {
+    if (err) throw err;
+  });
+
+  const SCAFFOLD = path.resolve(__dirname, '../../', 'scaffold/service');
+  const DEST_DIR = path.resolve(destination, directory);
+
+  mustache.escape = v => v;
+
+  new Scaffold({
+    data: {...pickBy(
+      (val, key) => startsWith('scaffold_', key),
+      { ...config, ...{
+        scaffold_project_name: name,
+        scaffold_project_description: description,
+        scaffold_project_author: author,
+        scaffold_project_git_url: giturl,
+        scaffold_project_directory: directory,
+        scaffold_mongodb_name: mongodb,
+        scaffold_service_port: port,
+      }}
+    )},
+    ignore: config.FILES_IGNORE,
+    render: mustache.render,
+  })
+  .copy(SCAFFOLD, DEST_DIR)
+  .then(() => {
+    console.log('done'); // eslint-disable-line no-console
+  })
+  .catch(e => {
+    console.log(e); // eslint-disable-line no-console
+  });
+}
