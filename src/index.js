619c337c65852988a331d608e623fee3282e159f
jabdul <james.abdul@craftturf.com>
2019-04-13 23:20:34 +0100

    Select endpoint or service skeleton to generate

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
 import inquirer from 'inquirer';
+import { Subject } from 'rxjs';
 
-import { generator as generateEndpoint, questions } from './endpoint/generator';
+import { generator as endpointGenerator, questions as endpointQuestions } from './endpoint/generator';
+import { generator as serviceGenerator, questions as serviceQuestions } from './service/generator';
+
+export const questions = {
+  selection: {
+    type: 'checkbox',
+    name: 'scaffold',
+    message: 'What would you like to scaffold?',
+    choices: [
+      {
+        name: 'Endpoint',
+        value: 'endpoint',
+      },
+      new inquirer.Separator(),
+      {
+        name: 'Microservice API',
+        value: 'service',
+      }
+    ]
+  },
+  endpoint: [ endpointQuestions, endpointGenerator ],
+  service: [ serviceQuestions, serviceGenerator ],
+};
 
 const build = async () => {
   try {
-    const { endpoint, endpoints, title, destination } = await inquirer.prompt(questions);
+    const prompts = new Subject();
 
-    generateEndpoint({ endpoint, endpoints, title, destination });
+    let scaffold;
+    let generator;
+    let answers = {};
+    let i = -1;
+
+    inquirer.prompt(prompts)
+      .ui.process.subscribe(
+        ({ answer, name }) => {
+          if (i == -1) {
+            scaffold = questions[answer][0];
+            generator = questions[answer][1];
+          }
+
+          i++;
+          answers[name] = answer;
+
+          if (scaffold.length !== i) prompts.next(scaffold[i]);
+          else prompts.complete();
+        },
+        err => {
+          console.warn(err); // eslint-disable-line no-console
+        },
+        () => {
+          generator(answers);
+          console.log('Code generation completed.👋\n'); // eslint-disable-line no-console
+        }
+      );
+    prompts.next(questions.selection);
   }
   catch(err) {
     console.log(err); // eslint-disable-line no-console
